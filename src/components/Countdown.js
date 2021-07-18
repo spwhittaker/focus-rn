5011c6153999591e23b9a5345730e9aa997c816a
Stephen Whittaker <stephenwhittaker23@gmail.com>
2021-07-18 15:40:46 +0100

    vibrate and keep awake

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -3,35 +3,49 @@ import { StyleSheet, Text, View } from "react-native";
 import { colors } from "../utils/colors";
 import { fontSizes, spacing } from "../utils/sizes";
 
-const minutestoMillis = (min) => min * 1000 * 60;
-const formatTime = (time) => (time < 10 ? `0${time}` : time);
-export function Countdown({ minutes = 20, isPaused }) {
+export const Countdown = ({ minutes = 20, isPaused, onProgress, onEnd }) => {
+  const minutesToMillis = (min) => min * 1000 * 60;
+  const [millis, setMillis] = useState(minutesToMillis(minutes));
+  const minute = Math.floor(millis / 60000) % 60;
+  const seconds = Math.floor(millis / 1000) % 60;
+  const formatTime = (time) => (time < 10 ? `0${time}` : time);
+
   const interval = useRef(null);
-  const countDown = () =>
+  const countDown = () => {
     setMillis((time) => {
       if (time === 0) {
+        clearInterval(interval.current);
+        onEnd();
+        return time;
       }
       const timeLeft = time - 1000;
+
+      onProgress(timeLeft / minutesToMillis(minutes));
       return timeLeft;
     });
+  };
+
+  useEffect(() => {
+    setMillis(minutesToMillis(minutes));
+  }, [minutes]);
 
   useEffect(() => {
     if (isPaused) {
+      if (interval.current) {
+        clearInterval(interval.current);
+      }
       return;
     }
     interval.current = setInterval(countDown, 1000);
     return () => clearInterval(interval.current);
-  }, [isPaused]);
-  const [millis, setMillis] = useState(minutestoMillis(minutes));
-  const minute = Math.floor(millis / 60000) % 60;
-  const seconds = Math.floor(millis / 1000) % 60;
+  }, [isPaused, minutes]);
 
   return (
     <Text style={styles.text}>{`${formatTime(minute)}:${formatTime(
       seconds
     )}`}</Text>
   );
-}
+};
 
 const styles = StyleSheet.create({
   text: {
--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,21 +1,67 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Vibration, Platform } from "react-native";
+import { ProgressBar } from "react-native-paper";
+import { useKeepAwake } from "expo-keep-awake";
 
 import { colors } from "../../utils/colors";
 import { fontSizes } from "../../utils/sizes";
 import { spacing } from "../../utils/sizes";
 import { Countdown } from "../../components/Countdown";
 import { RoundedButton } from "../../components/RoundedButton";
-export function Timer({ focusSubject }) {
+import { Timing } from "./Timing";
+
+const DEFAULT_TIME = 0.1;
+export const Timer = ({ focusSubject }) => {
+  useKeepAwake();
   const [isStarted, setIsStarted] = useState(false);
+  const [minutes, setMinutes] = useState(DEFAULT_TIME);
+  const [progress, setProgress] = useState(1);
+
+  const onProgress = (progress) => setProgress(progress);
+
+  const vibrate = () => {
+    if (Platform.OS === "ios") {
+      const interval = setInterval(Vibration.vibrate(), 1000);
+      setTimeout(() => clearInterval(interval), 10000);
+    } else {
+      Vibration.vibrate([1000, 1000, 1000, 1000]);
+    }
+  };
+
+  const onEnd = () => {
+    vibrate();
+    setMinutes(DEFAULT_TIME);
+    setProgress(1);
+    setIsStarted(false);
+  };
+  const changeTime = (min) => {
+    setMinutes(min);
+    setProgress(1);
+    setIsStarted(false);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.countdown}>
-        <Countdown isPaused={!isStarted} />
+        <Countdown
+          minutes={minutes}
+          isPaused={!isStarted}
+          onProgress={onProgress}
+          onEnd={onEnd}
+        />
       </View>
       <View style={{ paddingTop: spacing.xxl }}>
         <Text style={styles.title}>Focusing on:</Text>
         <Text style={styles.task}>{focusSubject}</Text>
+      </View>
+      <View style={{ paddingTop: spacing.sm }}>
+        <ProgressBar
+          progress={progress}
+          color="#5e84e2"
+          style={{ height: 10 }}
+        />
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Timing changeTime={changeTime} />
       </View>
       <View style={styles.buttonWrapper}>
         {isStarted ? (
@@ -26,7 +72,7 @@ export function Timer({ focusSubject }) {
       </View>
     </View>
   );
-}
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -44,7 +90,8 @@ const styles = StyleSheet.create({
   },
   buttonWrapper: {
     flex: 0.3,
-    padding: 15,
+    flexDirection: "row",
+    padding: spacing.md,
     justifyContent: "center",
     alignItems: "center",
   },
